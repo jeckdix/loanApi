236156eb11fe6c8361727f7d35eb53cc892d3455
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2024-01-16 15:34:22 +0100

    program

--- loanApi/Program.cs
+++ loanApi/Program.cs
@@ -1,8 +1,20 @@
+using loanApi.Data;
+using loanApi.Services.AccountInformations;
+using loanApi.Services.CardDetails;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add DbContext
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+           .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<ICardDetailsRepository, CardDetailsRepository>();
+builder.Services.AddScoped<IAccountInformationRepository, AccountInformationRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
