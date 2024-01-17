2d7b0fa491c23a4bb3050359d5026ee568aea5c8
faisl <faisal@gmail.com>
2024-01-17 00:12:25 -0800

    created the Database Entity and CalculateloanInterest

---
+++ loanApi/Helper/LoanCalculator.cs
@@ -0,0 +1,26 @@
+﻿using loanApi.Services.LoanType;
+
+namespace loanApi.Helper
+{
+    public class LoanCalculator
+    {
+        public static decimal CalculateInterest(decimal loanAmount, double interestRate, int durationMonths)
+        {
+            double monthlyInterestRate = interestRate / 100 / 12;
+            decimal monthlyPayment = loanAmount * (decimal)(monthlyInterestRate / (1 - Math.Pow(1 + monthlyInterestRate, - durationMonths)));
+            decimal totalRepayment = monthlyPayment * durationMonths;
+            return totalRepayment - loanAmount;
+        }
+
+        private bool IsLoanExpiredWithinYears(DateTime? expiryDate, int years)
+        {
+            // Assume the current date is used for comparison
+            if (expiryDate.HasValue && expiryDate.Value.Date < DateTime.UtcNow.Date.AddYears(-years))
+            {
+                return true; // Loan is expired
+            }
+
+            return false; // Loan is not expired
+        }
+    }
+}
