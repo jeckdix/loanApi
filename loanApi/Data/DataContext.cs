998685dca910f9d2f2df4658b685712ff9b2537f
SuleimanOzigis007 <151519069+SuleimanOzigis007@users.noreply.github.com>
2024-01-17 02:48:37 -0800

    created the Database Entity and CalculateloanInterest (#1)
    Co-authored-by: faisl <faisal@gmail.com>

--- loanApi/Data/DataContext.cs
+++ loanApi/Data/DataContext.cs
@@ -1,22 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using loanApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace loanApi.Data
 {
     public class DataContext : DbContext
 
     {
+        public DataContext() : base()
+        {
+        }
         public DataContext(DbContextOptions <DataContext> options) : base(options){}
 
+       
 
         //Line 10 above for user model
 
         //Line 12 above for cards model
 
         //Line 14 above for Account info model
-
+        public DbSet<LoanTypes> Loantypes { get; set; } 
         //line 16 above for Loan model 
 
     }
+    
 }
 
 
